fa46f38bafe828a240a4f460640953d736bbdef2
Archerist <yunuscemberci@gmail.com>
2020-10-22 00:29:34 +0300

    final commit?

--- app.js
+++ app.js
@@ -2,17 +2,59 @@ import * as DC from 'discord.js';
 
 const client = new DC.Client();
 
+const countdown_day = new Date(present_date.getFullYear(), 8, 17) 
+const one_day = 1000 * 60 * 60 * 24 
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
 
 client.on('message', msg => {
-    if ( msg.content.match(/.*cp77.*when.*/gi) ){
-        msg.channel.send("neva")
+    if ( msg.content.match(/.*(cp77|cyberpunk(|77)).*when.*/gi) ){
+        let present = new Date();
+        let result = (Math.round(countdown_day.getTime() - present_date.getTime()) / (one_day)).toFixed(0);
+
+        
+  
+        let status;
+            
+        if(Final_Result > 0) {
+        
+            status += 'There are ' + Final_Result +  ' days left until the release of #Cyberpunk2077 #CP2077 #2077';
+        }
+        else
+        {
+            //Final status
+            status += 'Wake up samurai. #Cyberpunk2077 releases today!';
+        }
+        
+        return status;
+        
+        msg.reply(status);        
+
+
     }
 });
 
 client.login(process.env.TOKEN);
 
-//deneme
+function countdownMessage() {
+  
+    let status;
+    
+    if(Final_Result > 0) {
+  
+        status += 'There are ' + Final_Result +  ' days left until the release of #Cyberpunk2077 #CP2077 #2077';
+    }
+    else
+    {
+        //Final status
+        status += 'Wake up samurai. #Cyberpunk2077 releases today!';
+    }
+    
+  return status;
+    
+};
+//19TH NOVEMBER 2020 AT 12:00AM
+
