af0e8f8662ea0b4d8ee560bf2333651c11df348e
Archerist <yunuscemberci@gmail.com>
2020-10-22 11:23:51 +0300

    timezones

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import * as DC from 'discord.js';
 import moment from 'moment';
 import 'moment-precise-range-plugin';
+import 'moment-timezone';
 
 const client = new DC.Client();
 
-const countdown_day = moment('2020-19-11 00:00:00', 'YYYY-DD-MM HH:mm:ss')
+const countdown_day = moment('2020-19-11 00:00:00', 'YYYY-DD-MM HH:mm:ss').tz('UTC-05:00')
 const one_day = 1000 * 60 * 60 * 24 
 
 client.on('ready', () => {
