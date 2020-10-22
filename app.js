7a226ff6693299fd52bf3b11ac80d7aed77e58ad
Archerist <yunuscemberci@gmail.com>
2020-10-22 11:00:15 +0300

    moment

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import * as DC from 'discord.js';
+import moment from 'moment';
+import 'moment-precise-range-plugin';
 
 const client = new DC.Client();
 
-const countdown_day = new Date(2020, 11, 19) 
+const countdown_day = moment('2020-19-11 00:00:00', 'YYYY-DD-MM HH:mm:ss')
 const one_day = 1000 * 60 * 60 * 24 
 
 client.on('ready', () => {
@@ -12,24 +14,14 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if ( msg.content.match(/.*(cp77|cyberpunk(|77)).*when.*/gi) ){
-        let present = new Date();
-        let result = (Math.round(countdown_day.getTime() - present.getTime()) / (one_day)).toFixed(0);
+        let present = moment();
+        let diff = moment.preciseDiff(countdown_day,present)
 
+        console.log(diff);
+        console.log(present);
         
   
-        let status = '';
-            
-        if(result > 0) {
         
-            status += 'There are ' + result +  ' days left until the release of CP77';
-        }
-        else
-        {
-            //Final status
-            status += 'Wake up samurai. #Cyberpunk2077 releases today!';
-        }
-        
-        msg.reply(status);        
 
 
     }
@@ -37,22 +29,5 @@ client.on('message', msg => {
 
 client.login(process.env.TOKEN);
 
-function countdownMessage() {
-  
-    let status;
-    
-    if(Final_Result > 0) {
-  
-        status += 'There are ' + Final_Result +  ' days left until the release of #Cyberpunk2077 #CP2077 #2077';
-    }
-    else
-    {
-        //Final status
-        status += 'Wake up samurai. #Cyberpunk2077 releases today!';
-    }
-    
-  return status;
-    
-};
 //19TH NOVEMBER 2020 AT 12:00AM
 
